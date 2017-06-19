11ac9ececa8e357b6c9a93b4b0a12e2e95f02c23
a-siddeley <asiddeley@rogers.com>
2017-06-19 07:55:15 -0400

    Moveable Handle post-merge

---
+++ features/Moveable.js
@@ -0,0 +1,172 @@
+/************************************************************
+	license:
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	
+	project:	BIM united FC (Feature Collection)
+	module: 	Moveable
+	desc: 
+	author: 	Andrew Siddeley 
+	started:	5-Jun-2017
+	
+****************************************************************/
+
+// Define a Module with Simplified CommonJS Wrapper...
+// see http://requirejs.org/docs/api.html#cjsmodule
+define( function(require, exports, module) {
+
+var $=require('jquery');
+var Feature=require('features/Feature');
+var TextFC=require('features/TextFC');
+var ChooserFC=require('features/ChooserFC');
+var Coaster=require('handles/Coaster');
+var coasterHandler=new Coaster();
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var Moveable=function(mesh, options){ 
+	/***************
+	Function that returns a fresh feature object {}, scoped to a particular mesh
+	mesh - scope or context of the feature
+	more - {} with additional data such as bimHandler, scene or whatever
+	****************/
+	Feature.call(this, mesh, options);
+	
+	if (typeof mesh.bimData.moveable=='undefined') {mesh.bimData.moveable='Off';}
+	
+	//override following
+	this.alias='moveable'; //property key
+	this.desc='Element can be moved to any point on the coaster';
+	this.control=ChooserFC; //requires choices
+	this.choices=['Off','XY yellow','YZ cyan', 'ZX magenta'];
+	this.coaster=null; //mesh movement stencil, a plane for pointer to intersect to get position 
+	this.mesh=mesh;
+	//this.propKey='moveable'; //TO-DO instead of this.alias
+	//this.propJar=mesh.bimData; //TO-DO instead of this.prop=... and this.mesh
+	this.prop=mesh.bimData.moveable; //prop - meant for display only 
+	this.propToBe=null; //proposed new value for property
+	this.propUpdate=function(propToBe){
+		//Feature.prototype.propUpdate.call(this, propToBe);
+		this.mesh.bimData[this.alias]=propToBe;
+		this.coasterManager(propToBe);
+	};
+	this.setScene=function(scene, mesh){
+		Feature.prototype.setScene(scene, mesh);	
+		return mesh;
+	};
+	//TO-DO create a dual variable FC, for choices & sizes
+	this.size=50; //size of coaster. 
+
+};
+
+//Inherit from prototype 
+Moveable.prototype=Object.create(Feature.prototype);
+Moveable.prototype.constructor=Moveable;
+
+//Override
+Moveable.prototype.coasterManager=function(propToBe){
+	//this function is called by this.control (chooserFC) when a choice is selected 
+	var data={mesh:this.mesh, moveable:this};
+	var tools=BIM.resources.tools;
+<<<<<<< HEAD
+	
+=======
+	BIM.fun.cameraPause(); //need to pause camera first
+>>>>>>> 81e369d20c9b2a1c93b4cef1a3eb6e0d69c19a78
+	var moveStart=function(ev){ 
+		console.log("moveStart");
+		var pickResult=BIM.scene.pick(
+			//point to test
+			BIM.scene.pointerX,	BIM.scene.pointerY, 
+			// predicate function for pickResult.hit
+			function(pickedmesh){ 
+				//console.log("pickedMesh/featureMesh="+pickedmesh.name+"/"+ev.data.mesh.name);
+				return (pickedmesh.name==ev.data.mesh.name);
+			} 
+		);
+		//console.log("hit "+pickResult.hit);
+		if (pickResult.hit) {
+			//BIM.fun.cameraPause();
+			$(BIM.options.canvas).on('mousemove.moveable', data, movement);
+		}
+		//return false;
+	}
+	
+	var movement=function(ev){ //event handler
+		//console.log('moveSome');
+		var pickResult=BIM.scene.pick(
+			BIM.scene.pointerX, BIM.scene.pointerY,  //point to test
+			// predicate function for pickResult.hit
+			function(mesh){ return (mesh==BIM.resources.tools.coaster);} 
+		);
+		//console.log(pickResult.hit);
+		if (pickResult.hit){
+			//console.log(BIM.scene.pointerX, BIM.scene.pointerY);
+			//move mesh to position where pointer hit coaster...
+			ev.data.mesh.position.copyFrom(pickResult.pickedPoint);
+		}
+		//return false to call event.stopPropagation() & event.preventDefault() 
+		//return false; 
+	};	
+
+	var moveStop=function(ev){
+		//console.log("moveStop");
+<<<<<<< HEAD
+		$(BIM.options.canvas).off('mousemove.moveable');
+		BIM.fun.cameraPlay(); //harmless - will have no effect unless cameraPaused
+		BIM.fun.trigger("propertychanged", [ev.data.mesh, "position"]);
+=======
+		BIM.fun.cameraPlay(); //harmless - will have no effect unless cameraPaused
+		//TO-DO trigger position changed to update
+		//$(BIM.options.canvas).off('mousemove.moveable');
+		//BIM.fun.trigger('featurechange', [ev.moveable]);
+		//TO-DO force choose Off so coaster can be disposed
+>>>>>>> 81e369d20c9b2a1c93b4cef1a3eb6e0d69c19a78
+		//return false;
+	};
+	
+	if (propToBe!='Off'){
+		//dispose existing coaster
+		if (tools.coaster!=null){
+			tools.coaster.dispose();
+			//tools.coasterHandle.dispose(tools.coaster);
+		}
+		//make new coaster
+		tools.coaster=tools.coasterHandle.setScene(BIM.scene, null, this.size, propToBe);	
+		tools.coaster.position.copyFrom(this.mesh.position);
+	
+		//Babylon action manager doesn't have a mousemove trigger so used jquery instead for all - 
+		//best not to mix jquery events and babylon action manager triggers to avoid conflicts
+		//Note use of namespace ie.moveable in events below per jquery best practices
+		$(BIM.options.canvas).on('mousedown.moveable', data, moveStart);
+		$(BIM.options.canvas).on('mouseup.moveable', data, moveStop);		
+	} else {
+		//Off selected...
+		//Ensure coaster exists before trying to dispose it
+		if (BIM.resources.tools.coaster!=null){
+			BIM.resources.tools.coasterHandle.dispose(BIM.resources.tools.coaster);
+		}
+		//following are harmless if event handlers don't exist
+		$(BIM.options.canvas).off('mousedown.moveable');
+		$(BIM.options.canvas).off('mousemove.moveable');
+		$(BIM.options.canvas).off('mouseup.moveable');
+	}
+};
+
+return Moveable;
+});
+
+
---
+++ handles/Handle.js
@@ -0,0 +1,116 @@
+/************************************************************
+	license:
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	
+	project:	BIM united FC (Feature & Function Collection)
+	module: 	__PartHandler
+	desc: 		Constructor function that creates a handle Object
+	author: 	Andrew Siddeley 
+	started:	8-May-2017
+	
+****************************************************************/
+// Define a Module with Simplified CommonJS Wrapper...
+// see http://requirejs.org/docs/api.html#cjsmodule
+define( function(require, exports, module){
+
+//var $=require('jquery');
+//var babylon=require('babylon');
+//var Nameable=require('features/Nameable');
+
+var Handle=function(moreFeatures){
+
+	//this.bimType='Handle';
+	this.desc='prototype for all Bim-United-FC scene-model elements';
+	if (Array.isArray(moreFeatures)){ this.Features=moreFeatures; }
+	else {this.Features=[];}
+	//console.log(this.Features);
+};
+var __=Handle.prototype;	
+
+__.setScene=function(scene, parentMesh){
+	//mesh - babylon mesh, optional
+	
+	if (typeof parentMesh == 'undefined'){parentMesh={};}
+	parentMesh.bimhandle=this; 
+<<<<<<< HEAD
+	parentMesh.bimData={};
+
+
+=======
+	parentMesh.bimData={childMeshes:[] };
+	//apply Features to handle...
+>>>>>>> 81e369d20c9b2a1c93b4cef1a3eb6e0d69c19a78
+	for (var i in this.Features){
+		//Features are constructor functions so run setScene from prototypes...
+		this.Features[i].prototype.setScene(scene, parentMesh);
+	}
+	return parentMesh;
+};
+
+__.addFeatures=function(){
+	//adds a feature constructor function to the handler being constructed
+	for (var a in arguments) {
+		this.Features.push(arguments[a]);
+		//console.log(arguments[a]);
+	}
+};	
+
+__.dispose=function(mesh){
+		//disposes mesh and any children, that is someMesh where someMesh.parent == mesh
+		var cc=mesh.getChildren();
+		for (var c in cc){cc[c].dispose();}
+		mesh.dispose();
+}
+
+__.getfeatures=function(mesh) {
+	/**********
+	Returns a fresh hash of features:
+	{name:{feature}, position:{feature}...}
+	A feature is a hash scoped to a particular mesh like this:
+	{label:'name', valu:mesh.variable, onFeatureChange:fn(ev,mesh,res){...}, editor:featureEditer}
+	**********/
+	//console.log('getfeatures(), this:');console.log(this);
+	//Hope there's no confusion with Features, features & feature.
+	var i, feature, features={};
+	for (i in this.Features){
+		feature=new this.Features[i](mesh);
+		features[feature.alias]=feature;
+	}
+	return features;
+}
+
+<<<<<<< HEAD
+return Handle;
+=======
+__.unite=function(parentMesh, mesh1, mesh2, mesh3, etc){
+	//Adds meshes to parentMesh.bimData
+	//usually called by inheritor's setScene()
+	//mesh refs are stored in bimData and unsed when disposing united meshes
+	for (var i=0; i<arguments.length; i++){
+		if (i==0){ 
+			parentMesh=arguments[i]; 
+		}
+		else {
+			parentMesh.bimData.childMeshes.push(arguments[i]);
+			//BABYLON parent mesh transfromations (translations, rotations etc) applied to each mesh
+			arguments[i].parent=parentMesh;
+		}
+	}
+}
+
+return Handler;
+>>>>>>> 81e369d20c9b2a1c93b4cef1a3eb6e0d69c19a78
+});
